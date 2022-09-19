8914de113be18af61a560f25bcb305174fb8e7f8
James Knight <contact@mtn-studio.com>
2022-09-18 20:52:03 -0700

    Added functions to populate display when digits are clicked. Clear and reset functions work but memory as well as operands currently don't.

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 TODO:
 
 1/ Build basic math operator functions (add, subtract, multiply, divide)
-2. Create new function (operate) that takes an operator function and 2 numbers.
-3. Basic html build for the calculator. Button for each digit, each of the basic math operators and an "Equals" key. Include a current value display and a "Clear" button.
-4. Create function to populate display when number buttons are clicked. Store "display value" in a variable.
+2/ Create new function (operate) that takes an operator function and 2 numbers.
+3/ Basic html build for the calculator. Button for each digit, each of the basic math operators and an "Equals" key. Include a current value display and a "Clear" button.
+4/ Create function to populate display when number buttons are clicked. Store "display value" in a variable.
 5. Create function that makes the calculator work!
 
 EXTRA: Style calculator once completed to use stock images to look like an actual calculator. Refer to images within /reference-img folder 
 */
+
+const display = document.getElementById('display'),
+	digitButtons = document.querySelectorAll('.digit'),
+	operatorButtons = document.querySelectorAll('.operator');
+
+let curDisplay = '',
+	storedDisplay = '';
 
 // Math operators
 function opPlus(x, y) {
@@ -29,3 +36,42 @@ function opDivide(x, y) {
 function operate(x, y, op) {
 	return op(x, y);
 }
+
+// Append number to display
+function appendNumber(num) {
+	if (display.textContent === '0') display.textContent = '';
+	curDisplay += num;
+	display.textContent += num;
+}
+// Clear display
+function clearDisplay() {
+	curDisplay = '';
+	display.textContent = '0';
+}
+// Reset memory
+function resetMemory() {
+	curDisplay = '';
+	storedDisplay = '';
+}
+
+// Event listeners
+digitButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		appendNumber(button.textContent);
+		console.log(`Current display value: ${curDisplay}`);
+	});
+});
+operatorButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		if (button.id == 'clear') {
+			clearDisplay();
+			console.log('Display cleared!');
+		}
+		if (button.id == 'reset') {
+			clearDisplay();
+			display.textContent = '0';
+			resetMemory();
+			console.log('Memory cleared!');
+		}
+	});
+});
